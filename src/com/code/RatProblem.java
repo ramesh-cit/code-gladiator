525b459e04795d192cb6ce101a032ab7736a9c22
ramesh-cit <ramesh.s@whitelotus.co.in>
2016-06-15 16:48:32 -0400

    Ramesh Changes

--- src/com/code/RatProblem.java
+++ src/com/code/RatProblem.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 import java.util.Arrays;
 
-public class CandidateCode {
+public class RatProblem {
 	public static void main(String[] args) {
 		String[] position = { "1#1", "2#5", "3#3", "6#3" };
 		String[] temperature = { "2#6#8#6#-7", "2#5#-5#-5#0", "-1#3#-8#8#7", "3#2#0#6#9", "2#1#-4#5#8", "-5#6#7#4#7" };
