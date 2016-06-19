fa9a475ad2913e060f404747f901be4f954fbdf1
ramesh <ramesh@linux-iqt6.fios-router.home>
2016-06-19 14:35:24 -0400

    Welding Job problem

---
+++ src/com/code/WeldingJobMachine.java
@@ -0,0 +1,18 @@
+package com.code;
+
+import java.io.*;
+
+public class WeldingJobMachine {
+
+	public static void main(String[] args) {
+		String[] input = { "6AM#8AM", "11AM#1PM","7AM#3PM" };
+		int output = jobMachine(input);
+	}
+
+	public static int jobMachine(String[] input1)
+
+	{
+		return 0;
+		// Write code here
+	}
+}
